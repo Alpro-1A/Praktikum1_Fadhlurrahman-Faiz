ed6cde8fb62ee18c20956b4e51755e58e1336cfa
FadhlurrahmanFaiz <fadhlurrahman.faiz6453@gmail.com>
2023-10-13 10:28:44 +0700

    menambahkan file .cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "---------------------------------------------------------------------------------------------------\n\n";
+    string NamaSaya;
+    NamaSaya = "Fadhlurrahman Faiz";
+    string NamaKelas;
+    NamaKelas = "1A Informatika";
+    string NPM;
+    NPM = "2310631170079";
+    string CitaCita;
+    CitaCita = "Programer Google";
+    string MotivasiSaya;
+    MotivasiSaya = "Prospek kerja";
+
+    cout << "Nama: " << NamaSaya << endl << endl;
+    cout << "Kelas: " << NamaKelas << endl << endl;
+    cout << "NPM: " << NPM << endl << endl;
+    cout << "Cita cita: " << CitaCita << endl << endl;
+    cout << "Motivasi: " << MotivasiSaya << endl << endl;
+    cout << "----------------------------------------------------------------------------------------------------\n\n";
+    return 0;
+
+}
